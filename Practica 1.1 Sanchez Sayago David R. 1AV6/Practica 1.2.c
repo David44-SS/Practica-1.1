6c2d47bdce21e1f79f581820d00475c84accecc7
David44-SS <55525146+David44-SS@users.noreply.github.com>
2019-09-23 15:51:35 -0500

    Add files via upload

---
+++ Practica 1.1 Sanchez Sayago David R. 1AV6/Practica 1.2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void)
+{
+printf("Practica 1(2)\n");
+printf("Sanchez Sayago David R.\n");
+printf("1.Suma \n");
+printf("2.Resta \n");
+printf("3.Multiplicacion\n");
+printf("Introduce una opcion \n");
+int opt;
+scanf("%d",&opt);
+int a;
+int b;
+printf("Introduce la primer cantidad \n");
+scanf("%d",&a);
+printf("Introduce la segunda cantidad \n");
+scanf("%d",&b);
+
+int c;
+if(opt==1){
+c=a+b;
+}
+else{
+if(opt==2){
+c=a-b;
+}
+else{ c=a*b;
+}
+printf("El resultado es:%d",c);
+}
+}
---
+++ Practica 1.1 Sanchez Sayago David R. 1AV6/Practica 1.4.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void)
+{
+printf("practica 1(3)\n");
+printf("Sanchez Sayago David R.\n");
+printf("1.Suma \n");
+printf("2.Resta \n");
+printf("3.Multiplicacion\n");
+printf("4.Division\n");
+printf("Introduce una opcion \n");
+int opt;
+scanf("%d",&opt);
+int a;
+int b;
+printf("Introduce la primer cantidad \n");
+scanf("%d",&a);
+printf("Introduce la segunda cantidad \n");
+scanf("%d",&b);
+int c;
+if(opt==1){
+c=a+b;
+}
+else{
+if(opt==2){
+c=a-b;
+}
+else{
+if (opt==3){
+    c=a*b;
+}
+else {
+ if (b==0){
+printf("No es posible dividir entre cero\n");
+ }
+ else{
+    c=a/b;
+}
+}
+}
+printf("El resultado es:%d",c);
+}
+}
+
